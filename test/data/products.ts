d7f3e659e854e0d4b754b02ea2a423813a2ae901
Andrés Felipe Bolaños <ghostafbr@gmail.com>
2024-07-12 18:30:49 -0500

    Update products.ts

--- test/data/products.ts
+++ test/data/products.ts
@@ -2,12 +2,12 @@ export const product1 = {
     id: '1',
     title: 'Coffee Mug - Card',
     // img: './coffee-mug.png',
-}
+};
 
 export const product2 = {
     id: '2',
     title: 'Coffee Mug - Card',
     img: './coffee-mug.png',
-}
+};
 
 export const products = [product1, product2];
